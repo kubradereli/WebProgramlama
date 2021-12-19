b521f4ede2f1a8fd5c392eec0bd40450406ceefc
kubradereli <0kubradereli@gmail.com>
2021-12-19 23:27:15 +0300

    Yorum ile ilgili kısımlar tamamlandı

---
+++ Project/Project/ViewComponents/CommentList.cs
@@ -0,0 +1,35 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using Project.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Project.ViewComponents
+{
+    public class CommentList : ViewComponent
+    {
+        public IViewComponentResult Invoke()
+        {
+            var commentvalues = new List<UserComment>
+            {
+                new UserComment
+                {
+                    ID = 1,
+                    Username = "Ali"
+                },
+                new UserComment
+                {
+                    ID = 2,
+                    Username = "Mehmet"
+                },
+                new UserComment
+                {
+                    ID = 3,
+                    Username = "Merve"
+                }
+            };
+            return View(commentvalues);
+        }
+    }
+}
